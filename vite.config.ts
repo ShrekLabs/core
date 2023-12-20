80d980946b5f5d2fe1f30c26a0327d59331c0c67
Ruminat <Blowlyge@gmail.com>
2023-12-21 00:26:45 +0300

    sus

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import dts from "vite-plugin-dts";
 import path from "path";
 import { defineConfig } from "vite";
+import packageJson from "./package.json";
 
 // https://github.com/jasonsturges/vite-typescript-npm-package
 
@@ -14,6 +15,9 @@ export default defineConfig({
       name: "yourname",
       formats: ["es", "cjs", "umd", "iife"],
       fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: Object.keys(packageJson.devDependencies || {}),
     },
   },
 });
